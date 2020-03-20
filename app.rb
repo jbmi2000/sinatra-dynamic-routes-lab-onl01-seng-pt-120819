ff81b81b7c4e2c3c0613e14dcb0a40dc300b4e22
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-20 02:02:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,8 +34,9 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    "#{@number1 + @number2}"
+    "#{@number1+@number2}"
     "#{@number2 - @number1}"
+  
   end
 
 
