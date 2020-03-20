e1147b5dfcb64482abeb3e6eb10aa518b1297120
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-20 02:34:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
 
     if @operation == "add"
-      @number1 + @number2
+      "#{@number1 + @number2}"
     elsif @operation == "subtract"
       @number2 - @number1
     elsif @operation == "multiply"
