dda30e3f9c29a7ff32d783c73b7d18801f2b37e4
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-20 01:51:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,5 +31,11 @@ class App < Sinatra::Base
 
   end
 
+  get '/:operation/:number1/:number2' do
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "#{@number1+@number2}"
+  end
+
 
 end
