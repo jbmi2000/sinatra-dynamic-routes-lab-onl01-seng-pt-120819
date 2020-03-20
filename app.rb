5ae21708dd5a52ec43d497b7296c5c2b90d074f0
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-20 01:21:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,9 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     "#{@number}"
-    "#{@phrase}"
+    phrase_array = []
+    @number.times {|n| phrase_array << @phrase}
+    phrase_array.join(" ")
   end
 
 end
